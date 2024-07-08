13339930709decd7faf9094687d4e8ab862752bd
Thomas antony <thomasantony12042001@gmail.com>
2024-07-08 12:01:39 +0530

    for windows

---
+++ main.py
@@ -0,0 +1,28 @@
+from pytube import YouTube
+import streamlit as st
+import os
+
+def Download(link):
+    try:
+        youtubeObject = YouTube(link)
+        youtubeObject = youtubeObject.streams.get_highest_resolution()
+        folder = os.path.expanduser("~")
+        default_download_folder = os.path.join(folder, "Downloads")
+        youtubeObject.download(output_path=default_download_folder)
+        st.write("Download is completed successfully to", default_download_folder)
+    except:
+        st.write("An error has occurred")
+
+
+def process_input(link):
+    # Process the input text here
+    st.write("Input Text:", link)
+
+st.title("YouTube video downloader")
+link = st.text_input("Enter the youtube link")
+
+if st.button("Download"):
+    process_input(link)
+    Download(link)
+
+
